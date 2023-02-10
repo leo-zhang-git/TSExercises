b09ae9b0390d3c3ea5304a075f085cebdf0de3d9
zhangwei <zw13272627539@dingtalk.com>
2023-02-10 17:42:45 +0800

    finish 15

--- test.ts
+++ test.ts
@@ -1,18 +1,5 @@
-let a = new Promise<string>((resolve, reject) => {
-    let flag = true
-    if(flag){
-        setTimeout(_=>{
-            resolve('1 ok')
-        }, 2000)
-    }
-    else{
-        reject(RangeError)
-    }
-})
-
-async function Test(){
-    let ret = await a
-    console.log(ret)
+function func(a:string, b: number){
+    console.log(a, b)
 }
-Test()
-// a.then((s)=>{console.log(s)}).catch(_=>{console.log("Faild")})
+
+func(1, 'sss')
